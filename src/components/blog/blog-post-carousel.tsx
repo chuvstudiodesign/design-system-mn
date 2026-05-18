9cc7063b3887251866b6aecfdff07dcbba7e517a
lucaszerlotini <lucaszerlotini@Mac-mini-de-lucaszerlotini.local>
2026-05-17 22:30:16 -0300

    Revert "Break carousel out to section bounds"
    This reverts commit 9deeaa133f410d00ed2b265557e90e9db9290c7c.

--- src/components/blog/blog-post-carousel.tsx
+++ src/components/blog/blog-post-carousel.tsx
@@ -18,21 +18,23 @@ export function BlogPostCarousel({
   basePath?: string;
 }) {
   return (
-    <div className="relative ml-[calc(var(--section-padding-x)*-1)] w-[calc(100%+var(--section-padding-x)*2)] overflow-hidden">
-      <Carousel opts={{ align: "start", loop: true }} className="w-full">
-        <CarouselContent className="-ml-4">
-          {posts.map((post) => (
-            <CarouselItem
-              key={post.id}
-              className="pl-4 basis-full md:basis-1/2 xl:basis-1/3 shrink-0"
-            >
-              <BlogPostCard post={post} basePath={basePath} />
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious className="left-3" />
-        <CarouselNext className="right-3" />
-      </Carousel>
+    <div>
+      <div className="relative">
+        <Carousel opts={{ align: "start", loop: true }} className="w-full">
+          <CarouselContent className="-ml-4">
+            {posts.map((post) => (
+              <CarouselItem
+                key={post.id}
+                className="pl-4 basis-full md:basis-1/2 xl:basis-1/3 shrink-0"
+              >
+                <BlogPostCard post={post} basePath={basePath} />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className="left-3" />
+          <CarouselNext className="right-3" />
+        </Carousel>
+      </div>
     </div>
   );
 }
